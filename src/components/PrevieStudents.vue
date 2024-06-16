128d1d0012be6559f376167755b6c1271a5dd88a
yoyan1 <yoyan947@gmail.com>
2024-06-15 23:32:46 -0600

    first commit with few file structuring and pages

---
+++ src/components/PrevieStudents.vue
@@ -0,0 +1,20 @@
+<template>
+    <q-card>
+      <q-card-section class="row items-center q-pb-none text-h6">
+        Dialog
+      </q-card-section>
+
+      <q-card-section>
+        This dialog has a backdrop filter of .
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn flat label="Close" color="primary" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </template>
+  
+  <script setup>
+
+  </script>
+  
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated >
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar.png">
+          </q-avatar>
+          RMES-SM
+        </q-toolbar-title>
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/img/avatar.png">
+        </q-avatar>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" bordered>
+      <q-list>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="dashboard" />
+          </q-item-section>
+          <q-item-section>Dashboard</q-item-section>
+        </q-item>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="group" />
+          </q-item-section>
+          <q-item-section>Students</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const leftDrawerOpen = ref(true)
+
+</script>
